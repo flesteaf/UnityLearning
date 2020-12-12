7d5c47f4f2d2ca0e443b10e14eedb45fd1b0877f
flesteaf <flesteaf@users.noreply.github.com>
2020-12-12 19:22:39 +0200

    [HACK-2] - finished Lesson 5.1

---
+++ Create With Code/Prototype5/Assets/Scripts/Target.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private const int maxTorque = 10;
+    private const int xRange = 4;
+    private const int yPos = -2;
+    private const int minForce = 12;
+    private const int maxForce = 16;
+    private Rigidbody targetRb;
+    private int RandomTorque => Random.Range(-maxTorque, maxTorque);
+    private Vector3 RandomXPosition => new Vector3(Random.Range(-xRange, xRange), yPos);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(RandomUpForce(), ForceMode.Impulse);
+
+        var torque = new Vector3(RandomTorque, RandomTorque, RandomTorque);
+        targetRb.AddTorque(torque, ForceMode.Impulse);
+
+        transform.position = RandomXPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown() 
+    {
+        Destroy(gameObject);    
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        Destroy(gameObject);    
+    }
+
+    private Vector3 RandomUpForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+}
